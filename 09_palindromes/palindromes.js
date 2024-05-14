39a7d08e01e0697bc25c82e7e1f5b156f0d1e190
baglana <102351104+baglana@users.noreply.github.com>
2024-05-14 08:02:21 +0500

    Add solutions to 08-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = removePunctuationAndSpace(str);
+  str = str.toLowerCase();
+  return str === str.split("").reverse().join("");
+};
 
+const removePunctuationAndSpace = function (str) {
+  const punctuationAndSpace = /[\.,?!\s]/g;
+  return str.replace(punctuationAndSpace, "");
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const ages = people.map(
+    (p) => (p.yearOfDeath ?? new Date().getFullYear()) - p.yearOfBirth
+  );
 
+  const sortedAges = [...ages].sort((a, b) => b - a);
+
+  const oldestIndex = ages.indexOf(sortedAges[0]);
+
+  return people[oldestIndex];
 };
 
 // Do not edit below this line
